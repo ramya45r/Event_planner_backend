57a9849b4db76f54e3a466352306d7c9304a7f5b
Ramya mol R <ramya45r@gmail.com>
2025-11-04 13:01:18 +0530

    mongodb  connected

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
 dotenv.config();
+ connectDB();
 
 
 
